1cc423d4bc884a74798705c2d57e506c890ac90f
MdSomad <mdsomad10@gmail.com>
2023-07-24 20:50:45 +0530

    edit

--- routes/user.js
+++ routes/user.js
@@ -32,25 +32,25 @@ routes.route("/login").post(login);
 
 routes.route("/logout").get(logout);
 
-routes.route("/follow/:id",).get(checkUserAuth,followUser);
+routes.route("/follow/:id").get(checkUserAuth,followUser);
 
-routes.route("/Fetch/followers/:id",).get(FetchAllFollowersById);
+routes.route("/Fetch/followers/:id").get(FetchAllFollowersById);
 
-routes.route("/Fetch/following/:id",).get(FetchAllFollowingById);
+routes.route("/Fetch/following/:id").get(FetchAllFollowingById);
 
-routes.route("/change/password",).put(checkUserAuth,changePassowrd);
+routes.route("/change/password").put(checkUserAuth,changePassowrd);
 
-routes.route("/update/profile",).put(checkUserAuth,updateProfile);
+routes.route("/update/profile").put(checkUserAuth,updateProfile);
 
-routes.route("/update/avater",).put(checkUserAuth,updateAvaterProfile);
+routes.route("/update/avater").put(checkUserAuth,updateAvaterProfile);
 
-routes.route("/delete/me",).delete(isAuthenticated,deleteMyProfile);
+routes.route("/delete/me").delete(isAuthenticated,deleteMyProfile);
 
-routes.route("/me",).get(checkUserAuth,myProfile);
+routes.route("/me").get(checkUserAuth,myProfile);
 
-routes.route("/user/:_id",).get(checkUserAuth,getUserProfile);
+routes.route("/user/:_id").get(checkUserAuth,getUserProfile);
 
-routes.route("/users",).get(checkUserAuth,getAllUsers);
+routes.route("/users").get(checkUserAuth,getAllUsers);
 
 routes.route("/forgot/password").post(forgotPassword);
 
